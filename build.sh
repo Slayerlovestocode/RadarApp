d351b30d20106ab138540a606c92b6a095e18279
Sudhanshu Awasthi <itsmeofficialsudhanshu@gmail.com>
2025-09-27 23:22:37 +0530

    wertyu

--- build.sh
+++ build.sh
@@ -8,6 +8,3 @@ pip3 install -r requirements.txt
 
 # Collect static files
 python3 manage.py collectstatic --no-input --clear # --clear is recommended for clean builds
-
-# Apply database migrations
-python3 manage.py migrate
