68a7280d10e6ff12d9ebd761d56c418e2ae7ab61
Sudhanshu Awasthi <itsmeofficialsudhanshu@gmail.com>
2025-09-27 23:06:00 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,6 +3,8 @@
 # Exit on error
 set -o errexit
 
+# Install dependencies
+pip install -r requirements.txt
 
 # Collect static files
-python manage.py collectstatic --no-input
+python3 manage.py collectstatic --no-input
